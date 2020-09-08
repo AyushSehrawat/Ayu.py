649b462be54b3f7a9bd80ab669ddbdbcfd82c142
Ayush Sehrawat <69469790+AyushSehrawat@users.noreply.github.com>
2020-09-08 18:33:23 +0530

    Fixed errors in calculating marks
    The script was calculating the wrong marks so introduced a new list which contains the computer wins

--- Codes/Rock-Paper-Scissor.py
+++ Codes/Rock-Paper-Scissor.py
@@ -1,26 +1,34 @@
 import random
 # Imported random to avoid same choice of computer
 import time
-# Imported time to make our code look a little goos
+# Imported time to make our code look a little good
 
 print('Welcome to Rock,Paper,Scissor game')
 
 name = input('Your name: ')
+print(f'Welcome {name}')
 turns = int(input('How many turns do you want?\n>>>'))
+print(f'{name} choose {turns} turns')
 
 options = ['r', 'p', 's']
 
 marks = []
 
+c_marks = []
+
 total = []
 
-# Created these two empty list to add user and computer choice respectively and calculate the marks
+# Created these three empty list to add user , computer's, all choices respectively
+
+# To calculate winner we use marks and c_marks list whereas to show user marks we use marks and total list
 
 
 # Here we will give user 10 turns
 for num in range(turns):
-    choice = input('(R)ock,(P)aper or (S)cissor\n1 for exit\n>>>').lower()
+    print('-'*40)
+    choice = input('(R)ock,(P)aper or (S)cissor\n1 for exit and 2 for help\n>>>').lower()
     computer = random.choice(options)
+    print('-'*40)
 
 # Now we will write all possible conditions and add user & computer decisions
     if choice == computer:
@@ -28,6 +36,7 @@ for num in range(turns):
         time.sleep(1)
         print('Tie!')
         marks.append(choice)
+        c_marks.append(computer)
         total.append(computer)
 
     elif choice == 'r':
@@ -44,6 +53,7 @@ for num in range(turns):
             time.sleep(1)
             print('Oops computer won!')
             total.append(computer)
+            c_marks.append(computer)
 
     elif choice == 'p':
 
@@ -59,6 +69,7 @@ for num in range(turns):
             time.sleep(1)
             print('Oops computer won!')
             total.append(computer)
+            c_marks.append(computer)
 
     elif choice == 's':
 
@@ -74,34 +85,42 @@ for num in range(turns):
             time.sleep(1)
             print('Oops computer won!')
             total.append(computer)
+            c_marks.append(computer)
 
     elif choice == '1':
         print(f':( {name} left the game in between')
         break
+
+    elif choice == '2':
+        print(f'Hey {name}\n | There are 3 choices for you |\n| (R)ock, (P)aper and (S)cissor |\n| Rock smashes scissor '
+              f'but is captured by paper |\n| Paper captures Rock but looses in front of Scissor |\n| Scissor kills '
+              f'Paper but '
+              f'Rock smashes Scissor |')
 
     else:
         print('Computer choosing...')
         time.sleep(1)
         print('oops something went wrong')
         total.append(computer)
+        c_marks.append(computer)
 
 
 # Now in the below code we will tell user the marks/his choices/computer choices/exit depending on the input
 
 while True:
-    print('-'*30)
+    print("~"*30)
 
-    score = input(f'1 - {name} marks\n2 - Show computer choices\n3 - Show {name} choice\n4- Exit\n>>>')
+    score = input(f'1 - {name} marks\n2 - Show computer wins\n3 - Show {name} wins\n4- Exit\n>>>')
 
     if score == '1':
         time.sleep(1)
         print(f'You got {len(marks)}/{len(total)}')
 
-        if len(marks) > len(total):
+        if len(marks) > len(c_marks):
             time.sleep(0.5)
             print(f'{name} won!')
 
-        elif len(marks) == len(total):
+        elif len(marks) == len(c_marks):
             time.sleep(0.5)
             print("It's a tie!")
 
@@ -111,19 +130,23 @@ while True:
 
     elif score == '2':
         time.sleep(1)
-        print(f'Computer choices are \n {total}')
+        print(f'Computer got {len(c_marks)} correct \n {c_marks}')
+        print('\nIncluding the tie')
 
     elif score == '3':
         time.sleep(0.5)
-        print(f'Your choices are \n {marks}')
+        print(f'Your got {len(marks)} correct \n {marks}')
+        print('\nIncluding the tie')
 
     elif score == '4':
         time.sleep(0.5)
-        print(f'Bye Bye {name}')
+        print(f'Bye! Waiting for you {name}')
         break
 
     else:
         time.sleep(0.5)
         print('Wrong input! Retry')
 
+
+# Used len(length of list) to find the marks of the user and used the list to show all choices
 # The end
