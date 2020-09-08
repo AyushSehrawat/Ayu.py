056772914efb60561c9bebd9224e5f3a421c191e
Ayush Sehrawat <69469790+AyushSehrawat@users.noreply.github.com>
2020-09-08 10:17:21 +0530

    Update discord_bot.py

--- Discord-Bot/discord_bot.py
+++ Discord-Bot/discord_bot.py
@@ -21,9 +21,6 @@ async def on_message(message):
     if message.content.startswith('$help'):
         await message.channel.send('Following are the commands\n$hello\n$joke')
 
-    if message.content.startswith('mental'):
-        await message.channel.send('Any kind of abuse is not allowed')
-
     if message.content.startswith('$joke'):
         joke = joke_api.get_joke()
         print(joke)
@@ -36,3 +33,17 @@ async def on_message(message):
 
 client.run('Your bot token here!')
 
+
+# Instructions-
+# Sign in or register to discord server!
+# Then create a guild/server
+# Then go to https://discord.com/developers/applications
+# Create a new application here with the name of your bot as the application!
+# Then you can also add a profile picture to your bot there!
+# Then go to OAuth2 tab in the portal and check bot button under the scope
+# A new tab will open! There give bot admin permissions
+# A link will be generated and then add it to your discord server or guild
+# Now the bot will be offline, to activate it do the following-
+# Go to bot tab in discord dev portal! Then find reveal token button. Click on it and it will reveal a token. Copy it and paste it in [client.run('Your bot token here!')].
+# Then open cmd and run discord file.
+# Boom! It will start your bot! Now you can edit the code accroding to you!
